d51b1ce8f87069cd451e134c80f4657e1c0a8cfe
Rafael Vastag <rgvastag@gmail.com>
2022-06-29 10:06:58 -0300

    Initial commit

---
+++ VastagDiscoveryServer/src/main/java/com/vastag/discovery/VastagDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.vastag.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VastagDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VastagDiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ VastagDiscoveryServer/src/test/java/com/vastag/discovery/VastagDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vastag.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VastagDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
